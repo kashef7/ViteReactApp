24564eb37ceb2951307ffff5ceb6c7c0909e98a5
kashef7 <11410120230477@stud.cu.edu.eg>
2025-05-07 14:02:45 +0300

    Added analytics and by the way vertical on phone still doesnt work

--- viteApp/src/main.jsx
+++ viteApp/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './css/index.css'
 import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics></Analytics>
     <App/>
   </StrictMode>,
 )
