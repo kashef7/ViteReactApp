32719d88335799bb8b589ca914007837153d6502
kashef7 <11410120230477@stud.cu.edu.eg>
2025-05-09 00:19:19 +0300

    fix img not showing when deploying

--- viteApp/src/components/NavBar.jsx
+++ viteApp/src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import "../css/NavBar.css"
-
+import profileIcon from '../assets/profile-svgrepo-com.svg';
+import menuIcon from '../assets/menu-svgrepo-com.svg';
 export default function NavBar(){
     return(
         <div className="navBar">
             <button className="Profile">
-                <img src="/src/assets/profile-svgrepo-com.svg"></img>
+                <img src={profileIcon}></img>
             </button>
             <button className="Menu">
-                <img src="/src/assets/menu-svgrepo-com.svg"></img>
+                <img src={menuIcon}></img>
             </button>
         </div>
     )
