74071f2e48f1c49ef83a499773b099759c90dca2
kashef7 <11410120230477@stud.cu.edu.eg>
2025-05-09 00:14:43 +0300

    Added Profile and Menu button to navBar

--- viteApp/src/components/NavBar.jsx
+++ viteApp/src/components/NavBar.jsx
@@ -3,6 +3,12 @@ import "../css/NavBar.css"
 export default function NavBar(){
     return(
         <div className="navBar">
+            <button className="Profile">
+                <img src="/src/assets/profile-svgrepo-com.svg"></img>
+            </button>
+            <button className="Menu">
+                <img src="/src/assets/menu-svgrepo-com.svg"></img>
+            </button>
         </div>
     )
 }
